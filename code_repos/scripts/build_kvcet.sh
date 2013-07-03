1465dd07560ec39f06100d3ad3d22e330c6eb04c
natu <natrajanm@hpadvise.com>
2013-07-04 01:24:55 +0530

    Adding the sfguarduser plugin

---
+++ code_repos/scripts/build_kvcet.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+env="$1"
+if [[ -z $env ]]
+then
+  echo "Usage : build_kvcet.sh env"
+else
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  cd $DIR
+  START=$(date +%s)
+  cd ..
+  ./symfony doctrine:drop-db --no-confirmation --env=$env
+  ./symfony doctrine:create-db --env=$env
+  ./symfony doctrine:build --all --env=$env
+  ./symfony doctrine:build --all-classes --env=$env
+  ./symfony doctrine:data-load --env=$env --append
+  END=$(date +%s)
+  DIFF=$(( $END - $START ))
+  echo "It took $DIFF seconds"
+fi
