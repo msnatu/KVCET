43103cfe0ac517dfe7e992134edda01e21e70cce
msnatu <natrajanm@hpadvise.com>
2013-08-09 00:45:30 +0530

    Enabling the fonts

--- code_repos/apps/frontend/templates/_head.php
+++ code_repos/apps/frontend/templates/_head.php
@@ -4,8 +4,8 @@
   <?php include_title() ?>
   <?php include_stylesheets() ?>
   <?php include_javascripts() ?>
-<!--  <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>-->
-<!--  <link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>-->
-<!--  <link href='http://fonts.googleapis.com/css?family=Capriola' rel='stylesheet' type='text/css'>-->
+  <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Capriola' rel='stylesheet' type='text/css'>
   <noscript>Your browser does not support Javascript!</noscript>
 </head>
