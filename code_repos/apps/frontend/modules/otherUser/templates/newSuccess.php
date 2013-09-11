df1eeefebf4b3093eff38d681b8982badf21bc50
Uday Shankar Singh <udayshankar1306@gmail.com>
2013-09-11 14:09:43 -0700

    1. Removed the manageUser Model 2. Changed the otherUser Form as per the discussion 3. Added more fixtures for otherUser, HOD and Staff's 4. Created Gmail Account for sending mail id is "noreply.kvcet@gmail.com" 5. Added the validation for otherUser

---
+++ code_repos/apps/frontend/modules/otherUser/templates/newSuccess.php
@@ -0,0 +1,31 @@
+<?php include_partial('global/page_title', array('title' => 'Adding New User', 'back_url' => 'otherUser/index', 'icon_class' => 'page-title-user-icon')); ?>
+
+<?php if ($sf_user->hasFlash('successMsg')): ?>
+<div class="flash_success"><?php echo $sf_user->getFlash('successMsg'); ?></div>
+<?php endif ?>
+
+<?php if ($sf_user->hasFlash('errorMsg')): ?>
+<div class="flash_error"><?php echo $sf_user->getFlash('errorMsg'); ?></div>
+<?php endif ?>
+    
+<?php
+$form = new formHelper();
+?>
+
+<div class="form-container">
+    <div class="form-header">User Details</div>
+    <form action="<?php echo url_for('otherUser/new'); ?>" method="POST" id="create_new">
+        <?php echo $form->printTextBox('First Name', 'first_name', '', 1, 2); ?>
+        <?php echo $form->printTextBox('Last Name', 'last_name', '', 1, 2); ?>
+        <?php echo $form->printTextBox('User E-mail', 'user_mail', '', 1); ?>
+        <?php echo $form->printSelectBox("add", "Assign to Department", "user_dept", $collgDept['options'], $collgDept['values'], 'a Department To Assign', "", 1); ?>
+        <?php echo $form->printSelectBox("add", "User Type", "user_group", $collgGroup['options'], $collgGroup['values'], 'a Group To Assign', "", 1); ?>
+        <input type="submit" value="Save & Proceed" name="save" class="form-submit-button">
+    </form>
+</div>
+
+<script type="text/javascript">
+  $(document).ready( function() {
+    $("#create_new").validationEngine();
+  });
+</script>
